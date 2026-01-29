a60c558a731c9fe586350006450bf88c53e5d79a
Mohammed Musthafa <mohamedmusthafa3679@gmail.com>
2026-01-29 20:17:05 +0400

    //

--- Isometric-Game-Server/obj/Debug/net10.0/Isometric-Game-Server.AssemblyInfo.cs
+++ Isometric-Game-Server/obj/Debug/net10.0/Isometric-Game-Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Isometric-Game-Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+66083649d28c8b831788f0e2295f6ea895e80515")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b6010f483ce76809b680b08f8e062b99cdf05f1b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Isometric-Game-Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Isometric-Game-Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
